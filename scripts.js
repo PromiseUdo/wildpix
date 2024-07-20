ba434eb9835d6056676b2350fc65349b05fb06d5
PromiseUdo <38910392+PromiseUdo@users.noreply.github.com>
2024-07-21 00:40:49 +0100

    update file paths

--- scripts.js
+++ scripts.js
@@ -50,10 +50,4 @@ document.addEventListener("DOMContentLoaded", () => {
       closeLightbox();
     }
   });
-
-  // Dark mode toggle
-  const themeSwitch = document.getElementById("theme-switch");
-  themeSwitch.addEventListener("change", () => {
-    document.body.classList.toggle("dark-theme", themeSwitch.checked);
-  });
 });
